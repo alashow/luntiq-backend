004a499a9479a7394f6bee6a84da413a89fba32d
Alashov Berkeli <yunus.alashow@gmail.com>
2018-08-25 10:14:35 -0500

    downloader: premiumize url multiplier

--- app/Util/Downloader/Aria2Client.php
+++ app/Util/Downloader/Aria2Client.php
@@ -11,9 +11,13 @@ class Aria2Client
     protected $defaultParams = [
         'allow-overwrite'           => 'true',
         'auto-file-renaming'        => 'false',
-        'max-connection-per-server' => '4',
         'file-allocation'           => 'none',
+        'max-connection-per-server' => '6',
+        'split'                     => '6',
     ];
+
+    protected $premUrlPattern = '/(https:\/\/[a-zA-Z]{1,20})(-)(sng1|fra1|nyc1|sfo1|tor1|sto)(.*)/';
+    protected $premCdnLocations = ['sng1', 'fra1', 'nyc1', 'sfo1', 'tor1', 'sto'];
 
     /**
      * Aria2Client constructor.
@@ -55,7 +59,7 @@ class Aria2Client
     {
         $requests = [];
         foreach ($files as $file) {
-            $uris = is_string($file['url']) ? [$file['url']] : $file['url'];
+            $uris = $this->multiplyUrl($file['url']);
             $requests[] = $this->jsonRpcClient->request(1, 'aria2.addUri', [
                 "token:{$this->token}", $uris,
                 [
@@ -65,7 +69,6 @@ class Aria2Client
             ]);
         }
         $responses = $this->jsonRpcClient->sendAll($requests);
-
 
         return $this->transformResponses($responses, function ($index) use ($files) {
             return $files[$index]['key'];
@@ -110,5 +113,26 @@ class Aria2Client
         $responses = $this->jsonRpcClient->sendAll($requests);
 
         return $this->transformResponses($responses);
+    }
+
+    /**
+     * If url matches premiumize cdn url format, then this function generates an url to the file for multiple cdn location.
+     * Otherwise, it just returns given url.
+     *
+     * @param string|array $url
+     *
+     * @return array|string
+     */
+    private function multiplyUrl($url)
+    {
+        preg_match($this->premUrlPattern, $url, $matches);
+        if (! empty($matches)) {
+            $urls = [];
+            foreach ($this->premCdnLocations as $location) {
+                $urls[] = preg_replace($this->premUrlPattern, "$1$2$location$4", $url);
+            }
+            return $urls;
+        }
+        return $url;
     }
 }
