4e5f17fd1317fd9c1a37027a9e80661be44a6e9d
Alashov Berkeli <yunus.alashow@gmail.com>
2019-05-25 15:40:34 -0500

    add: speed in bits

--- resources/assets/js/utils/filters.js
+++ resources/assets/js/utils/filters.js
@@ -8,10 +8,10 @@ Vue.filter("tmdbImage", (path, size) => {
 });
 
 Vue.filter("bytes", (bytes) => humanFileSize(bytes));
-Vue.filter("bytesSpeed", (bytes) => humanFileSize(bytes) + '/s');
+Vue.filter("bytesSpeed", (bytes) => bytesToSpeed(bytes));
 
 function humanFileSize(bytes, si) {
-    var thresh = si ? 1000 : 1024;
+    const thresh = si ? 1000 : 1024;
     if (Math.abs(bytes) < thresh) {
         return bytes + ' B';
     }
@@ -22,4 +22,19 @@ function humanFileSize(bytes, si) {
         ++u;
     } while (Math.abs(bytes) >= thresh && u < units.length - 1);
     return bytes.toFixed(1) + ' ' + units[u];
+}
+
+function bytesToSpeed(bytes) {
+    bytes = bytes * 8;
+    const thresh = 1024;
+    if (Math.abs(bytes) < thresh) {
+        return bytes + 'b/s';
+    }
+    const units = ['kbit', 'mbit', 'gbit'];
+    let u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u] + '/s';
 }
